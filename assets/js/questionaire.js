43aedb5953ac8e9657affe48afb3c1ca3bb3489b
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-04-03 16:17:45 +0000

    add structure

---
+++ assets/js/questionaire.js
@@ -0,0 +1,505 @@
+var quizQuestionsDataBase = [
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+    {
+        question:"add here",
+        answers: [
+            {answer: "ADD"},
+            {answer: "add"},
+            {answer: "ADD"},
+            {answer: "add"}
+        ],
+        correctAnswer: "add",
+    },
+]
+
+export {quizQuestionsDataBase};
